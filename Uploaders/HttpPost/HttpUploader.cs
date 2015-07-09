6997b110eb4da2df797123c0d9562c7bafbef695
Johan Geluk <jhgeluk@gmail.com>
2015-07-09 12:59:57 +0200

    Allow other plugins to use the API Fixed basic auth.

--- Uploaders/HttpPost/HttpUploader.cs
+++ Uploaders/HttpPost/HttpUploader.cs
@@ -15,15 +15,15 @@ namespace zSnap.Uploaders.HttpPost
     public class HttpPostUploader : ImageUploader, IConfigurableUploader
     {
         public new Image Image { get; private set; }
-        private Random rand = new Random();
 
         public HttpPostUploader()
         {
 
         }
 
-        private string RandomString(int length = 10)
+        private static string RandomString(int length = 10)
         {
+            var rand = new Random();
             const string validChars = "abcdefghijklmnopqrstuvwxyz0123456789";
             return new string(new char[length].Select(c => c = validChars[rand.Next(0, validChars.Length)]).ToArray());
         }
@@ -35,55 +35,102 @@ namespace zSnap.Uploaders.HttpPost
 
         public override bool Upload(out Uri location)
         {
-            using (var client = new HttpClient())
+            HttpResponseMessage response;
+            if (Configuration.UseBasicAuth)
             {
-                var form = new MultipartFormDataContent();
-                if (Configuration.UseBasicAuth)
+                response = UploadFile(Image, RandomString() + ".png", Configuration.Destination, Configuration.Username, Configuration.Password);
+            }else
+            {
+                response = UploadFile(Image, RandomString() + ".png", Configuration.Destination);
+            }
+            
+            try
+            {
+                response.EnsureSuccessStatusCode();
+            }
+            catch (HttpRequestException ex)
+            {
+                Notifications.Raise("The remote server returned an error: " + ex.Message, NotificationType.Error);
+                location = null;
+                return false;
+            }
+            var result = response.Content.ReadAsStringAsync().Result;
+            if (Configuration.UseRegex)
+            {
+                var rgx = new Regex(Configuration.Regex);
+                var match = rgx.Match(result);
+                if (match.Success)
                 {
-                    var credentials =
-                        Convert.ToBase64String(
-                            Encoding.UTF8.GetBytes(Configuration.Username + ":" + Configuration.Password));
-                    client.DefaultRequestHeaders.Add("Authorization", credentials);
-                }
-                var ms = new MemoryStream();
-                Image.Save(ms, ImageFormat.Png);
-                ms.Position = 0;
-                form.Add(new StreamContent(ms), "screenshot", RandomString() + ".png");
-                var response = client.PostAsync(Configuration.Destination, form).Result;
-
-                try
-                {
-                    response.EnsureSuccessStatusCode();
-                }
-                catch (HttpRequestException ex)
-                {
-                    Notifications.Raise("The remote server returned an error: " + ex.Message, NotificationType.Error);
-                    location = null;
-                    return false;
-                }
-                var result = response.Content.ReadAsStringAsync().Result;
-                if (Configuration.UseRegex)
-                {
-                    var rgx = new Regex(Configuration.Regex);
-                    var match = rgx.Match(result);
-                    if (match.Success)
-                    {
-                        location = new Uri(match.Groups["url"].Value);
-                    }
-                    else
-                    {
-                        Clipboard.SetText(result);
-                        Notifications.Raise("Unable to parse the image URL from the response.\nPress Ctrl+V to paste the full response returned by the server.", NotificationType.Error);
-                        location = null;
-                        return false;
-                    }
+                    location = new Uri(match.Groups["url"].Value);
+                    return true;
                 }
                 else
                 {
-                    location = new Uri(result);
+                    Clipboard.SetText(result);
+                    Notifications.Raise("Unable to parse the image URL from the response.\nPress Ctrl+V to paste the full response returned by the server.", NotificationType.Error);
+                    location = null;
+                    return false;
                 }
             }
-            return true;
+            else
+            {
+                location = new Uri(result);
+                return true;
+            }
+        }
+
+        /// <summary>
+        /// Uploads an image using an HTTP POST request without using HTTP Basic Authentication.
+        /// </summary>
+        /// <param name="image"></param>
+        /// <param name="filename"></param>
+        /// <param name="remoteHost"></param>
+        /// <returns></returns>
+        public static HttpResponseMessage UploadFile(Image image, string filename, string remoteHost)
+        {
+            return UploadFile(image, filename, remoteHost, null, null);
+        }
+
+        /// <summary>
+        /// Uploads an image using an HTTP POST request, using HTTP Basic Authentication.
+        /// </summary>
+        /// <param name="image">The image file to be uploaded.</param>
+        /// <param name="filename">The filename under which the image should be uploaded.</param>
+        /// <param name="remoteHost">The URL to send the request to</param>
+        /// <param name="username">The username, or null if auth should not be used.</param>
+        /// <param name="token">The token or password, or null if auth should not be used.</param>
+        /// <returns></returns>
+        public static HttpResponseMessage UploadFile(Image image, string filename, string remoteHost, string username, string token)
+        {
+            bool useAuth;
+            if(username == null && token == null)
+            {
+                useAuth = false;
+            }
+            else if (username == null || token == null)
+            {
+                throw new ArgumentException("You must supply a username and a token, or no credentials at all.");
+            }
+            else
+            {
+                useAuth = true;
+            }
+
+            using (var client = new HttpClient())
+            {
+                var form = new MultipartFormDataContent();
+                if (useAuth)
+                {
+                    var credentials = Convert.ToBase64String(Encoding.UTF8.GetBytes(username + ":" + token));
+                    client.DefaultRequestHeaders.Add("Authorization", "Basic " + credentials);
+                }
+                var ms = new MemoryStream();
+                image.Save(ms, ImageFormat.Png);
+                ms.Position = 0;
+                form.Add(new StreamContent(ms), "screenshot", filename);
+                var response = client.PostAsync(remoteHost, form).Result;
+                return response;
+            }
         }
 
         public override string Name
